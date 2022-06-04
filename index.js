d5db50ac44e6291bfd05e730276f5bf6dc34a812
tran vinh <tranvinhlx135@gmail.com>
2022-06-04 17:57:13 +0700

    adsfa

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
-import http from "http";
 
 const app = express();
 const port = process.env.PORT || 8000;
-const server = http.createServer(app);
 
 app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-async function query(data) {
-  const response = await fetch(
-    "https://api-inference.huggingface.co/models/gpt2",
-    {
-      headers: { Authorization: `Bearer ${process.env.API_TOKEN}` },
-      method: "POST",
-      body: JSON.stringify(data),
-    }
-  );
-  const result = await response.json();
-  return result;
-}
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/summarization", async (req, res) => {
   const response = await fetch(
@@ -53,6 +38,6 @@ app.use("*", (req, res) => {
   res.status(404).json({ error: "not found" });
 });
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log("Server is running on port", port);
 });
